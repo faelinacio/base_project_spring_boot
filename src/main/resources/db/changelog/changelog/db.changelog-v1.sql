128355007e4674a85dc3d230a8139bd12d11edff
Rafael Santos <faelinacio@gmail.com>
2022-04-21 15:48:11 -0300

    first commit and DB connection

---
+++ src/main/resources/db/changelog/changelog/db.changelog-v1.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset Liquibase Rafael:1
+
+create table "user"
+(
+    id       serial primary key,
+    name     varchar(255) not null,
+    email    varchar(255) not null,
+    password varchar(255) not null
+)
